6100f91178e7b18558fbff7bf8186f10bf05dc44
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-08-14 03:45:33 -0400

    Added license, authors files; added copyright comments to source files

--- path.go
+++ path.go
@@ -1,3 +1,7 @@
+// Copyright 2012 The Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // The bfspath package implements a breadth-first-search-based pathfinding 
 // algorithm which runs faster than Dijkstra's algorithm in certain cases.
 // This algorithm assumes that all edges have integral length. (Note that 
